bb08755701f52d3207fcf4a8868541520b1c28a7
Maël <mael.boisselot@gmail.com>
2024-08-13 12:16:08 +0200

    ajout modal edition/suppression

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/account", "Compte", "/pages/account.html"),
+    new Route("/editPassword", "Modifier le mot de passe", "/pages/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
